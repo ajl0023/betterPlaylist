e9315a2aa53975c9594689d4a6b93e06cb21a92b
austin <austinlee199723@yahoo.com>
2021-05-07 16:04:37 -0700

    l

--- server/start.js
+++ server/start.js
@@ -1,5 +1,5 @@
 const mongoUtil = require("./mongoUtil");
 mongoUtil.connect().then((client) => {
   const app = require("./index.js");
-  app.listen(process.env.PORT || 5000, function () {});
+  app.listen(process.env.PORT || 5000);
 });
