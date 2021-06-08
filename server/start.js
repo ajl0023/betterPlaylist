cd9c1581b567a72d6ff7175425079f32c4dc6e6a
austin <austinlee199723@yahoo.com>
2021-06-08 16:34:15 -0700

    error connecting to mongo on vercel

--- server/start.js
+++ server/start.js
@@ -1,5 +1,6 @@
 const mongoUtil = require("./mongoUtil");
 mongoUtil.connect().then((client) => {
+  console.log(client, 50);
   const app = require("./index.js");
   app.listen(process.env.PORT || 5000);
 });
