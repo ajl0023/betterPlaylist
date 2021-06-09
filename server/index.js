ec2c372bd6bbe2b3fa56d83dd55277de98640c8e
austin <austinlee199723@yahoo.com>
2021-06-08 17:07:11 -0700

    edited express static folder

--- server/index.js
+++ server/index.js
@@ -1,4 +1,7 @@
 const mongoUtil = require("./mongoUtil");
+const express = require("express");
+
+const path = require("path");
 mongoUtil.connect().then((client) => {
   const app = require("./server.js");
   app.listen(process.env.PORT || 5000);
