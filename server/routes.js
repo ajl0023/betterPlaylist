efcadf185c5dd854617f447ef2065560cc7ba31d
austin <austinlee199723@yahoo.com>
2021-06-17 20:28:44 -0700

    updated .env mongo

--- server/routes.js
+++ server/routes.js
@@ -33,7 +33,7 @@ module.exports = function (app) {
         redirect_uri:
           req.headers["x-forwarded-host"] === "localhost:3000"
             ? "http://localhost:3000"
-            : "https://better-playlist-git-main-ajl0023.vercel.app",
+            : "https://better-playlist.vercel.app",
       },
     })
       .then((data) => {
